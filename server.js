ac86b7c4f7a7b70ae8dce5d5b63e605e73c9c1a5
ALgoveia131 <alysson.goveia131@gmail.com>
2020-08-03 16:55:51 +0000

    main layout

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
-var logger = require("morgan"),
-cors = require("cors"),
-http = require("http"),
-express = require("express"),
-bodyParser = require("body-parser"),
-mongoose = require('mongoose');
+require('./models/db');
+
+const express = require(express);
+
+const path = require ('path');
+const exphbs = require ('express-handlebars'); //
+
+
+const jobController = require ('./controllers/job-controller');
 
 var app = express();
-var port = 3000;
-var userCtrl = require('./user-controller');
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(require('./routes'));
+app.set ('views', path, join(__dirname, '/views/'));
+app. engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname +'views/layouts/'}));
 
-app.listen(port, function(err){
-    console.log("Listening on Port: " + port)
+app.listen(3000, () =>{
+    console.log('Express server started at port : 3000');
 });
 
-mongoose.connect('mongodb://localhost/test');
-mongoose.connection.on('error', (err) => { 
-    console.log('Mongodb Error: ', err); 
-    process.exit();
-});
-mongoose.connection.on('connected', () => { 
-    console.log('MongoDB is successfully connected');
-});
+app.use('/jobs', jobController);
